66a87cccb25b9940d300d12cc7deb40c344cb2f0
daniel21h <hesseldaniel21@gmail.com>
2020-07-02 18:44:21 -0300

    feat: Add ChannelMessage component and finished

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>
+
+  useEffect(() => {
+    const div = messagesRef.current
+    
+    if (div) {
+      div.scrollTop = div.scrollHeight
+    }
+  }, [messagesRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage 
+          author="Daniel Hessel"
+          date="21/02/2020"
+          content="Hojé é seu aniversário."
+        />
+        ))}
+
+        <ChannelMessage 
+          author="Diego Fernandes"
+          date="21/02/2020"
+          content={
+            <>
+              <Mention>@Daniel Hessel</Mention>, bora levantar um server na aws?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre" />
